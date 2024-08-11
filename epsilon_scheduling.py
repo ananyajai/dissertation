3914c8d0c926af2d7f7346b1ae0a2216d3d293b7
ananyajai <ananyajshankar@gmail.com>
2024-08-11 15:17:00 +0100

    linear eps decay

--- epsilon_scheduling.py
+++ epsilon_scheduling.py
@@ -1,20 +1,12 @@
-def epsilon_decay(strat, timestep, max_timestep, eps_start=1.0, eps_min=0.05, eps_decay=0.9):
-
-    if strat == 'constant':
-        epsilon = 0.9
-
-    if strat == 'linear':
-        decay_steps = eps_decay * max_timestep
-        epsilon = max(eps_min, eps_start - (eps_start - eps_min) * min(1.0, timestep / decay_steps))
-
-    if strat == 'exponential':
-        epsilon = max(eps_min, eps_decay*epsilon)
+def linear_epsilon_decay(timestep, max_timestep, eps_start=0.97, eps_min=0.2, eps_decay=0.9):
+    decay_steps = eps_decay * max_timestep
+    epsilon = max(eps_min, eps_start - (eps_start - eps_min) * min(1.0, timestep / decay_steps))
 
     return epsilon
 
 
-# strat = 'linear'
-# max_timestep = 100
-# eps_decay = 0.8
+max_timestep = 50
+eps_decay = 0.8
 
-
+# for i in range(0, max_timestep+1):
+#     print(linear_epsilon_decay(i, max_timestep))  
